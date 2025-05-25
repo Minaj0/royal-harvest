e589834f451ae5f87083665342feb1aa80ca9065
Minaj <you@example.com>
2025-05-25 21:55:35 +0530

    changed about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,6 @@ export const About = () => {
                 <div className="hero-content flex flex-col justify-center lg:px-20">
                     <h1 className='text-4xl lg:text-5xl font-bold font-main text-sec'>About</h1>
                     <p className='text-sec font-main text-justify lg:text-3xl'>
-                        About Us
                         Welcome to Royal Harvest and Herbs Co. — where purity meets tradition.
                         At Royal Harvest and Herbs Co., we bring you the finest selection of premium dry fruits and aromatic spices sourced from the most trusted farms and suppliers around the world. Rooted in quality, authenticity, and integrity, our mission is to deliver nature’s bounty to your doorstep — fresh, flavorful, and full of nourishment.
                         We believe in the power of wholesome food. That’s why every almond, pistachio, saffron strand, and spice seed we offer is carefully handpicked, hygienically packed, and quality-checked to meet the highest standards. Whether you’re a health-conscious individual, a culinary enthusiast, or a retailer seeking bulk supply, we are your trusted partner for natural and nutritious products.
